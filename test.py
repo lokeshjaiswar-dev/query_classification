9fd4c247e7b5eee517fdb7e363bfe189a42e98a6
Lokesh Jaiswar <lokeshjaiswar.dev@gmail.com>
2026-08-06 00:22:53 +0530

    understanding dict in python

--- test.py
+++ test.py
@@ -1,25 +1,6 @@
-import json
+import request
 
-person = {
-    "name": "John Doe",
-    "age": 30,
-    "city": "New York",
-    "addr":{
-        "street": "123 Main St",
-        "zip": "10001"
-    }
-}
-print("Running")
-# print(json.dumps(person, indent=4))
-# print(json.loads(person))
+data = request.get("https://jsonplaceholder.typicode.com/todos")
 
-print(person.keys())
-print(person.values())
-print(person.items())
-
-for (key, value) in person.items():
-    if isinstance(value, dict):
-        print(f"{key}:")
-        for (sub_key, sub_value) in value.items():
-            print(f"  {sub_key}: {sub_value}")
-    print(f"{key}: {value}")
+print(data)
+print(data.json())
